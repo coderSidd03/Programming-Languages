5a2120a703fc96a5aacb9ba43b99388b4ae8848b
Fear Reaper <97720811+coderSidd03@users.noreply.github.com>
2022-08-06 02:33:56 +0530

    Create Hells Kitchen.js

---
+++ JavaScript/CodeWars/Hells Kitchen.js
@@ -0,0 +1,18 @@
+/*
+Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+Rules:
+
+Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+*/
+
+let str = 'What feck damn cake'
+
+function gordon (string) {
+    return string.toUpperCase().replace(/[A]/g,'@').replace(/[EIOU]/g,'*').split(` `).map(x => x+'!!!').join` `
+}
+
+console.log(gordon(str))
